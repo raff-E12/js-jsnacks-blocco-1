012cd909da9c53add0065eabf6e857dfff3239f0
Raff_12 <raffelecoppola381@gmail.com>
2025-02-27 12:52:01 +0100

    feat: Fine del primo esercizio da consegnare

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,2 +1,13 @@
 //Snack 1° : L’utente inserisce due numeri in successione, con due prompt.
 // Il software stampa il maggiore.
+
+const Number_1 = Number(window.prompt('Scegli il tuo primo numero?', ''));
+const Number_2 = Number( window.prompt('scegli il tuo secondo numero', ''));
+
+if (isNaN(Number_1)  < isNaN(Number_2)) {
+    console.log("Il numero che hai scelto è maggiore del'altro");
+} else {
+    console.log("Il numero che hai scelto è minore del'altro");
+}
+
+//ecco la  soluzione del mio problema
