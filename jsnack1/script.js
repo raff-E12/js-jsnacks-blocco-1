87e0bb64079089a2147a6b658f0aeed71311d74b
Raff_12 <raffelecoppola381@gmail.com>
2025-02-27 16:40:13 +0100

    feat: Correzione del primo esercizio

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,10 +4,12 @@
 const Number_1 = Number(window.prompt('Scegli il tuo primo numero?', ''));
 const Number_2 = Number( window.prompt('scegli il tuo secondo numero', ''));
 
-if (isNaN(Number_1)  < isNaN(Number_2)) {
-    console.log("Il numero che hai scelto è maggiore del'altro");
-} else {
-    console.log("Il numero che hai scelto è minore del'altro");
+if (isNaN(Number_1) < isNaN(Number_2)) {
+    console.log(`Il numero è maggiore di ${Number_1}`);
+} else if(isNaN(Number_1) > isNaN(Number_2)) {
+    console.log(`Il numero è maggiore di ${Number_2}`);
+} else{
+    console.log('Riprova ad inserire i numeri in maniera giusta.');
 }
 
 //ecco la  soluzione del mio problema
